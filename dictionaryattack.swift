e06c0d7744b320c6928189ff4617ac30435a2aba
Stephanholly <sholly@usc.edu>
2016-10-28 10:45:27 -0700

    first commit

---
+++ dictionaryattack.swift
@@ -0,0 +1,48 @@
+// DO NOT ALTER THE FOLLOWING DICTIONARY
+var capitals: [String : String] = [
+    "arizona" : "Scottsdale",
+    "california" : "Sacramento",
+    "texas" : "Austin",
+    "florida" : "Tallassee",
+    "colorado" : "Denver",
+    "michigan" : "Lansing",
+    "hawaii" : "honolulu",
+    "utah" : "Salt Lake City"
+]
+// return the keys of the dictionary
+
+
+
+
+
+
+// return the values of the dictionary
+
+
+
+
+
+// add oregon and its capital, Salem to the dictionary and return the dictionary
+
+
+
+
+
+
+// delete utah entirely from the dictionary
+
+
+
+
+
+// upcase the hawaii capital
+
+
+
+
+
+
+// update arizona capital to Phoenix
+
+
+
